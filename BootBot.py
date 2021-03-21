5f35e9c02f113a2506600a3845bbc0889a94b617
Katie Anna Dittrich <katieanna.dittrich@gmail.com>
2021-03-21 16:07:53 +0100

    Added statistics

--- BootBot.py
+++ BootBot.py
@@ -13,6 +13,13 @@ class MyClient(discord.Client):
 
         if message.content.lower().startswith('$hello'):
             await message.channel.send('Hello!')
+            await message.author.send('hello, how are you?')
+        if message.content.lower().startswith('$stats'):
+            counter = 0
+            async for m in message.channel.history(limit=50):
+                if m.author != self.user and m.content.lower() == '$hello':
+                    counter = counter + 1
+            await message.channel.send('There are ' + str(counter) + ' $hellos')
 
 
 client = MyClient()
