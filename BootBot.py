1d88ca362735d37423331d136d5ab43baed71869
Katie Anna Dittrich <katieanna.dittrich@gmail.com>
2021-04-24 18:17:52 +0200

    Updated code layout and added send message

--- BootBot.py
+++ BootBot.py
@@ -13,13 +13,18 @@ class MyClient(discord.Client):
 
         if message.content.lower().startswith('$hello'):
             await message.channel.send('Hello!')
-            await message.author.send('hello, how are you?')
+
         if message.content.lower().startswith('$stats'):
-            counter = 0
+            counter_all = 0
+            counter_author = 0
             async for m in message.channel.history(limit=50):
                 if m.author != self.user and m.content.lower() == '$hello':
-                    counter = counter + 1
-            await message.channel.send('There are ' + str(counter) + ' $hellos')
+                    counter_all = counter_all + 1
+                    if m.author == message.author:
+                        counter_author = counter_author + 1
+
+            await message.channel.send('There are ' + str(counter_all) + ' $hellos. ')
+            await message.channel.send('You send ' + str(counter_author) + ' $hellos. ')
 
 
 client = MyClient()
